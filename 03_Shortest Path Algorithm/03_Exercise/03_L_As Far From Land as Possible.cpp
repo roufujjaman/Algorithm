e26a90aaef6413ef40662836b26dd90de2cc5aa8
Roufujjaman <rrhtcode@gmail.com>
2023-09-14 23:00:14 +0600

    14 9 23

--- 03_Shortest Path Algorithm/03_Exercise/03_L_As Far From Land as Possible.cpp
+++ 03_Shortest Path Algorithm/03_Exercise/03_L_As Far From Land as Possible.cpp
@@ -1,28 +1,44 @@
-#include<bits/stdc++.h>
-using namespace std;
-const int N = 100;
-int visited[N][N];
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
 
-int main()
-{
-    int n, m;
-    cin >> n >> m;
-    queue<pair<int, int>> sources;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
+        queue<pair<int, int>> sources;
+        for (int i = 0; i < n; i++)
         {
-            int val;
-            cin >> val;
-            if ( val == 1 && val == 1) sources.push({i, j});
+            for (int j = 0; j < m; j++)
+            {
+                if (grid[i][j] == 1) sources.push({i, j});
+            }
         }
-    }
-    
-    while (!sources.empty())
-    {
+        vector<pair<int, int>> direction = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int level = -1;
+        while (!sources.empty())
+        {
+            level++;
+            int q_size = sources.size();
+            
+            while (q_size--)
+            {
+                int i = sources.front().first;
+                int j = sources.front().second;
+                sources.pop();
 
-    }
+                for (pair<int, int> d: direction)
+                {
+                    int ni = i + d.first;
+                    int nj = j + d.second;
 
-    
-    return 0;
-}
+                    if (ni < 0 || ni >= n || nj < 0 || nj >= m || grid[ni][nj] == 1)
+                    {
+                        continue;
+                    }
+                    grid[ni][nj] = 1;
+                    sources.push({ni, nj});
+                }
+            }
+        }
+        return (level == 0 || level == -1)? -1: level;
+    }
+};
