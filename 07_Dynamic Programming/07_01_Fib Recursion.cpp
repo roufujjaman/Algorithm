326877cf13a66b660a78db3df4210af6d0f4567d
Roufujjaman <rrhtcode@gmail.com>
2023-10-05 23:44:48 +0600

    5 10 23

--- 07_Dynamic Programming/07_01_Fib Recursion.cpp
+++ 07_Dynamic Programming/07_01_Fib Recursion.cpp
@@ -6,6 +6,8 @@ int fibonacci(int n)
 }
 int main()
 {
-    ;
+    int input_val;
+    cin >> input_val;
+    fibonacci(input_val);
     return 0;
 }
--- sketch.cpp
+++ sketch.cpp
@@ -1,7 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
+int fibonacci(int n)
+{
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    int first = fibonacci(n - 1);
+    int second = fibonacci(n - 2);
+    return first + second;
+}
 int main()
 {
-    ;
+    int input_val;
+    cin >> input_val;
+    cout << fibonacci(input_val);
     return 0;
 }
