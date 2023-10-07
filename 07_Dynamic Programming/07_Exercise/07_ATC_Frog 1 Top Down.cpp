7cdb3065151fca18a0987580ca75cdc5bb92d7b7
Roufujjaman <rrhtcode@gmail.com>
2023-10-07 22:42:40 +0600

    7 10 23

--- 07_Dynamic Programming/07_Exercise/07_ATC_Frog 1 Top Down.cpp
+++ 07_Dynamic Programming/07_Exercise/07_ATC_Frog 1 Top Down.cpp
@@ -1,15 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-int sum = 0;
+const int N = 1e5 + 4;
+int memo[N];
+void init_memo(int n)
+{
+    for (int i = 0; i <= n; i++)
+    {
+        memo[i] = -1;
+    }
+}
 int jump(int arr[], int n)
 {
     if (n == 1) return 0;
-    else if (n == 2) return abs(arr[n] - arr[n -1]);
+    else if (memo[n] != -1) return memo[n];
+    else if (n == 2)
+    {   
+        return memo[n] = abs(arr[n] - arr[n - 1]); 
+    }
     else
     {
         int left = abs(arr[n] - arr[n - 1]) + jump(arr, n - 1);
         int right = abs(arr[n] - arr[n - 2]) + jump(arr, n - 2);
-        return min(left, right);
+        return memo[n] = min(left, right);
     }
 }
 int main()
@@ -17,6 +29,7 @@ int main()
     int n;
     cin >> n;
     int arr[n + 1];
+    init_memo(n);
     for (int i = 1; i <= n; i++)
     {
         cin >> arr[i];
