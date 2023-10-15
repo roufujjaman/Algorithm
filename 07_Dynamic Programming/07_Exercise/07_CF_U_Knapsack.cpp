03b4dd4d8bc8e9c2c50b85d1bbfd2829015df41d
Roufujjaman <rrhtcode@gmail.com>
2023-10-15 23:25:44 +0600

    15 10 23

--- 07_Dynamic Programming/07_Exercise/07_CF_U_Knapsack.cpp
+++ 07_Dynamic Programming/07_Exercise/07_CF_U_Knapsack.cpp
@@ -1,30 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int knapsack(int position, int container_size, int weight[], int val[])
-{
-    // basecase
-    if (position == 0 || container_size == 0) return 0;
 
-    //
-    if (weight[position] <= container_size)
-    {
+int knapsack(int position, int container_size, int weight[], int val[]) {
+    // Base case
+    if (position < 1 || container_size == 0) return 0;
+
+    // If the weight of the current item can be accommodated
+    if (weight[position] <= container_size) {
         int withCurrent = knapsack(position - 1, container_size - weight[position], weight, val) + val[position];
         int withoutCurrent = knapsack(position - 1, container_size, weight, val);
         return max(withCurrent, withoutCurrent);
-    }
-    else
-    {
+    } else {
         return knapsack(position - 1, container_size, weight, val);
     }
 }
-int main()
-{
+
+int main() {
     int count, container;
     cin >> count >> container;
     int weight[count + 1];
     int val[count + 1];
-    for (int i = 1; i <= count; i++)
-    {
+    for (int i = 1; i <= count; i++) {
         cin >> weight[i];
         cin >> val[i];
     }
