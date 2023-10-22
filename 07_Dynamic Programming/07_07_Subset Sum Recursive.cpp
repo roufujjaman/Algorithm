b138624c4475ad14bcd6179f9867831abe25d5da
Roufujjaman <rrhtcode@gmail.com>
2023-10-22 21:45:07 +0600

    22 10 23

--- 07_Dynamic Programming/07_07_Subset Sum Recursive.cpp
+++ 07_Dynamic Programming/07_07_Subset Sum Recursive.cpp
@@ -1,39 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
-int steps = 0;
+const int N = 1e3;
+vector<int> memo(N, -1);
 bool fn(int n, int s, int arr[])
 {
-    steps++;
-    cout << n << " " << s << '\n';
-    if (n < 0)
+    if (n == 0)
     {
         if (s == 0) return true;
         else return false;
     }
-    if (arr[n] <= s)
+    if (memo[n - 1] != -1) return memo[n];
+    if (arr[n - 1] <= s)
     {
-        bool withCurrent = fn(n - 1, s - arr[n], arr);
+        bool withCurrent = fn(n - 1, s - arr[n - 1], arr);
         bool withoutCurrent = fn(n - 1, s, arr);
-        return withCurrent || withoutCurrent;
+        return memo[n - 1] = withCurrent || withoutCurrent;
     }
     else 
     {
-        return fn(n - 1, s, arr);
+        return memo[n - 1] = fn(n - 1, s, arr);
     }
 }
 int main()
 {
     int n;
     cin >> n;
-    int arr[n];
+    int arr[N];
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
     int s;
     cin >> s;
-    (fn(n - 1, s, arr))? cout << "YES" : cout << "NO";
-    cout << "\n" << steps;
+    (fn(n, s, arr))? cout << "YES" : cout << "NO";
     return 0;
 }
 
