0e9d4fafc81d2fd47e5167d634cfe9b82666ccd8
Roufujjaman <rrhtcode@gmail.com>
2023-10-25 23:59:26 +0600

    25 10 23

---
+++ 07_Dynamic Programming/07_Exercise/07_L_416. Partition Equal Subset Sum.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += nums[i];
+        }
+        if (sum % 2 == 0)
+        {
+            int target = sum / 2;
+            bool memo[n + 1][target + 1];
+            for (int i = 0; i <= target; i++)
+            {
+                if (i == 0) memo[0][i] = 1;
+                else memo[0][i] = 0;
+            }
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 0; j <= target; j++)
+                {
+                    if (nums[i - 1] <= j)
+                    {
+                        memo[i][j] = memo[i - 1][j - nums[i - 1]] || memo[i - 1][j];
+                    }
+                    else
+                    {
+                        memo[i][j] = memo[i - 1][j];
+                    }
+                }
+            }
+            if (memo[n][target]) cout << "true";
+            else cout << "false";
+        }
+        else
+        {
+            cout << "false";
+        }
+        return true;
+    }
+};
