8df33a21680f0a37b96165471a5551623262e420
Roufujjaman <rrhtcode@gmail.com>
2023-10-29 23:24:16 +0600

    29 10 23

--- sketch.cpp
+++ sketch.cpp
@@ -1,42 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
+vector<int> ss;
+void fn(int n, int arr[])
+{
+    if (n == 0)
+    {
+        ss.push_back(NULL);
+        return;
+    }
+}
 int main()
 {
-    int n, sum;
-    cin >> n >> sum;
+    int n;
+    cin >> n;
     int arr[n];
-    for (int i = 0; i <n ; i++)
-    {
-        cin >> arr[i];
-    }
-    int memo[n + 1][sum + 1];
-    memo[0][0] = 1;
-    for (int i = 1; i <= sum; i++)
-    {
-        memo[0][i] = 0;
-    }
-    int mod = 1e9 + 7;
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 0; j <= sum; j++)
-        {
-            if (arr[i - 1] <= j)
-            {
-                memo[i][j] = (memo[i - 1][j - arr[i - 1]] + memo[i - 1][j]);// % mod;
-            }
-            else
-            {
-                memo[i][j] = memo[i - 1][j];// % mod;
-            }
-        }
-    }
-    for (int i = 0; i <= n; i++)
-    {
-        for (int j = 0; j <= sum; j++)
-        {
-            cout << memo[i][j] << " ";
-        }
-        cout << "\n";
-    }
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    for (int i = 0; i < ss.size(); i++) cout << ss[i];
     return 0;
 }
