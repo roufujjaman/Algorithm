e88ad4e7f56547baaf1f89acec96cb93688abf56
Roufujjaman <rrhtcode@gmail.com>
2023-11-05 01:10:07 +0600

    5 11 23

---
+++ 08_Dynamic Programming/08_04_01_Coin Change.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int val[n];
+    for (int i = 0; i < n; i++) cin >> val[i];
+    int s;
+    cin >> s;
+    int mem[n + 1][s + 1];
+    mem[0][0] = 1;
+    for (int j = 1; j <= s; j++) mem[0][j] = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j <= s; j++)
+        {
+            if (val[i - 1] <= j)
+            {
+                mem[i][j] = mem[i][j - val[i - 1]] + mem[i - 1][j];
+            }
+            else
+            {
+                mem[i][j] = mem[i - 1][j];
+            }
+        }
+    }
+    cout << mem[n][s];
+    return 0;
+}
+
+// 3
+// 1 2 3
+// 5
--- sketch.cpp
+++ sketch.cpp
@@ -2,9 +2,6 @@
 using namespace std;
 int main()
 {
-    int n;
-    cin >> n;
-    int coin[n];
-    for (int i = 0; i < n; i++) cin >> coin[i];
+    ;
     return 0;
 }
