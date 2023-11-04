40e8be3dcf764323459f7c6b7c73ab5ace428152
Roufujjaman <rrhtcode@gmail.com>
2023-11-04 15:13:46 +0600

    4 11 23

--- sketch.cpp
+++ sketch.cpp
@@ -1,29 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N = 1e3;
-int mem[N][N];
-void init_mem(int n, int s)
-{
-    for (int i = 0; i <= n; i++)
-    {
-        for (int j = 0; j <= s; j++)
-        {
-            mem[i][j] = -1;
-        }
-    }
-}
-int fn(int n, int s, int val[], int weight[])
-{
-    if (n == 0 || s == 0) return 0;
-    if (mem[n][s] != -1) return mem[n][s];
-    if (weight[n - 1] <= s)
-    {
-        int left = val[n - 1] + fn(n, s - weight[n - 1], val, weight);
-        int right = fn(n - 1, s, val, weight);
-        return mem[n][s] = max(left, right); 
-    }
-    return mem[n][s] = fn(n - 1, s, val, weight);
-}
 int main()
 {
     int n, w;
@@ -31,11 +7,29 @@ int main()
     int val[n], weight[n];
     for (int i = 0; i < n; i++) cin >> val[i];
     for (int i = 0; i < n; i++) cin >> weight[i];
-    init_mem(n, w);
-    cout << fn(n, w, val, weight);
+    
+    int mem[n + 1][w + 1];
+    // basecase
+    for (int i = 0; i <= n; i++) mem[i][0] = 0; // weight   is 0 therefore no item can be taken
+    for (int j = 0; j <= w; j++) mem[0][j] = 0; // item     is 0 therefore no item to take
+    // loop starts from 1 & 1
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= w; j++)
+        {
+            if (weight[i - 1] <= j)
+            {
+                mem[i][j] = max(val[i - 1] + mem[i][j - weight[i - 1]], mem[i - 1][j]);
+            }
+            else
+            {
+                mem[i][j] = mem[i - 1][j];
+            }
+        }
+    }
+    cout << mem[n][w];
     return 0;
 }
-
 
 // 4 6
 // 5 3 2 4
