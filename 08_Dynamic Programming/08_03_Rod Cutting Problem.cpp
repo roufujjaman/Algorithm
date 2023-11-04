a368e6802ce4fd01c4a11caafb4858e17ddeccec
Roufujjaman <rrhtcode@gmail.com>
2023-11-04 23:31:16 +0600

    4 11 23

--- 08_Dynamic Programming/08_03_Rod Cutting Problem.cpp
+++ 08_Dynamic Programming/08_03_Rod Cutting Problem.cpp
@@ -2,6 +2,33 @@
 using namespace std;
 int main()
 {
-    ;
+    int n;
+    cin >> n;
+    int val[n];
+    for (int i = 0; i < n; i++) cin >> val[i];
+
+    int mem[n + 1][n + 1];
+    // basecase
+    for (int i = 0; i <= n; i++) mem[i][0] = 0; // weight   is 0 therefore no item can be taken
+    for (int j = 0; j <= n; j++) mem[0][j] = 0; // item     is 0 therefore no item to take;
+    // loop starts form 1, 1
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n ; j++)
+        {
+            if (i <= j)
+            {
+                mem[i][j] = max(val[i - 1] + mem[i][j - i], mem[i - 1][j]);
+            }
+            else
+            {
+                mem[i][j] = mem[i - 1][j];
+            }
+        }
+    }
+    cout << mem[n][n];
     return 0;
 }
+
+// 8
+// 2 4 4 5 8 10 12 9
--- sketch.cpp
+++ sketch.cpp
@@ -2,35 +2,9 @@
 using namespace std;
 int main()
 {
-    int n, w;
-    cin >> n >> w;
-    int val[n], weight[n];
-    for (int i = 0; i < n; i++) cin >> val[i];
-    for (int i = 0; i < n; i++) cin >> weight[i];
-    
-    int mem[n + 1][w + 1];
-    // basecase
-    for (int i = 0; i <= n; i++) mem[i][0] = 0; // weight   is 0 therefore no item can be taken
-    for (int j = 0; j <= w; j++) mem[0][j] = 0; // item     is 0 therefore no item to take
-    // loop starts from 1 & 1
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= w; j++)
-        {
-            if (weight[i - 1] <= j)
-            {
-                mem[i][j] = max(val[i - 1] + mem[i][j - weight[i - 1]], mem[i - 1][j]);
-            }
-            else
-            {
-                mem[i][j] = mem[i - 1][j];
-            }
-        }
-    }
-    cout << mem[n][w];
+    int n;
+    cin >> n;
+    int coin[n];
+    for (int i = 0; i < n; i++) cin >> coin[i];
     return 0;
 }
-
-// 4 6
-// 5 3 2 4
-// 4 1 3 2
