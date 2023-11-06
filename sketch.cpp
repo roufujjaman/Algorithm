e11b6afc3fd2a20deeb63b9bd903e7abb8b10a53
Roufujjaman <rrhtcode@gmail.com>
2023-11-06 23:42:31 +0600

    6 11 23

--- sketch.cpp
+++ sketch.cpp
@@ -9,15 +9,15 @@ int main()
     int s;
     cin >> s;
     int mem[n + 1][s + 1];
-    mem[0][0] = 0;
-    for (int j = 1; j <= s; j++) mem[0][j] = INT_MAX - 1;
+    mem[0][0] = 1;
+    for (int j = 1; j <= s; j++) mem[0][j] = 0;
     for (int i = 1; i <= n; i++)
     {
         for (int j = 0; j <= s; j++)
         {
             if (val[i - 1] <= j)
             {
-                mem[i][j] = min(1 + mem[i][j - val[i - 1]], mem[i - 1][j]);
+                mem[i][j] = mem[i][j - val[i - 1]] + mem[i - 1][j];
             }
             else
             {
@@ -25,6 +25,15 @@ int main()
             }
         }
     }
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= s; j++)
+        {
+            cout << mem[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
     cout << mem[n][s];
     return 0;
 }
