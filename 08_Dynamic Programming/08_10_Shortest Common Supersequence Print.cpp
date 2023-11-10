2e08dacb0767fd04fc8b21b6e51eabe99ce60424
Roufujjaman <rrhtcode@gmail.com>
2023-11-10 15:20:01 +0600

    10 11 23

---
+++ 08_Dynamic Programming/08_10_Shortest Common Supersequence Print.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s1, s2;
+    cin >> s1 >> s2;
+    int n = s1.size();
+    int m = s2.size();
+    int mem[n + 1][m + 1];
+    for (int i = 0; i <= n; i++) mem[i][0] = 0;
+    for (int j = 0; j <= m; j++) mem[0][j] = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (s1[i - 1] == s2[j - 1])
+            {
+                mem[i][j] = 1 + mem[i - 1][j - 1];
+            }
+            else
+            {
+                mem[i][j] = max(mem[i - 1][j], mem[i][j - 1]);
+            }
+        }
+    }
+    cout << (n + m)- mem[n][m];
+    return 0;
+}
+
+// COMMONALL
+// CONALL
+
--- sketch.cpp
+++ sketch.cpp
@@ -29,3 +29,4 @@ int main()
 
 // COMMONALL
 // CONALL
+
